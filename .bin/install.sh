06c332bea362665db127c9cdc7c50a6a58df415c
mmmarceleza <58913502+mmmarceleza@users.noreply.github.com>
2023-11-11 18:10:27 -0300

    small improvements on install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -4,12 +4,13 @@ function config {
 }
 mkdir -p $HOME/.config-backup
 cd $HOME
-config checkout
+config checkout 2>/dev/null
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
-    echo "Backing up pre-existing dot files.";
+    echo "Backing up pre-existing dot files to $HOME/.config-backup/";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
+echo "Reload your shell"
