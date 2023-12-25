7e4541e86a64ef23e6ce40ac21ff22abf10f3595
mmmarceleza <58913502+mmmarceleza@users.noreply.github.com>
2023-12-25 12:06:31 -0300

    fixing install.sh based on shellcheck

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -1,15 +1,16 @@
-git clone --bare https://github.com/mmmarceleza/dotfiles.git $HOME/.dotfiles
+#!/bin/bash
+
+git clone --bare https://github.com/mmmarceleza/dotfiles.git "$HOME"/.dotfiles
 function config {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" "$@"
 }
-mkdir -p $HOME/.config-backup
-cd $HOME
-config checkout 2>/dev/null
-if [ $? = 0 ]; then
+mkdir -p "$HOME"/.config-backup
+cd "$HOME" || { echo "Failure"; exit 1; }
+if [ "$(config checkout 2>/dev/null)" ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files to $HOME/.config-backup/";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
